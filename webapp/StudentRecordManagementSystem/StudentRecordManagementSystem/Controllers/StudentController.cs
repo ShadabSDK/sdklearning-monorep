7b2e071f6cdd03b5d667ae2e8e1898de2c2a2baa
DESKTOP-GF67IHK\admin <shadabgimt2006@gmail.com>
2024-02-11 13:13:14 +0530

    Create Student Method Added

---
+++ webapp/StudentRecordManagementSystem/StudentRecordManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentRecordManagementSystem.Models;
+
+namespace StudentRecordManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //- Only to get View and Show Form
+            return View();
+        }
+
+        // POST: Student/Create 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                StudentDataAccessLayer studentdal = new StudentDataAccessLayer();
+                studentdal.AddStudent(student);
+
+                //Once  Student Record Created it will redirect to Index Page
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ webapp/StudentRecordManagementSystem/StudentRecordManagementSystem/Models/StudentDataAccessLayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace StudentRecordManagementSystem.Models
+{
+    public class StudentDataAccessLayer
+    {
+        public void AddStudent(Student student)
+        {
+            using (SqlConnection con = new SqlConnection("Server=DESKTOP-GF67IHK\\SQLEXPRESS;Database=StudentManagement;Trusted_Connection=True;"))
+            {
+                SqlCommand cmd = new SqlCommand("spAddStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", student.LastName);
+                cmd.Parameters.AddWithValue("@Email", student.Email);
+                cmd.Parameters.AddWithValue("@Mobile", student.Mobile);
+                cmd.Parameters.AddWithValue("@Address", student.Address);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
