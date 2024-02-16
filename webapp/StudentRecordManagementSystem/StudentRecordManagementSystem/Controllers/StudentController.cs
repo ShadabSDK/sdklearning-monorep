d89fcb4c03fa43ed7333e5cd79a13c3af60c1fa9
DESKTOP-GF67IHK\admin <shadabgimt2006@gmail.com>
2024-02-16 18:44:49 +0530

    Complete ADO.NET

--- webapp/StudentRecordManagementSystem/StudentRecordManagementSystem/Controllers/StudentController.cs
+++ webapp/StudentRecordManagementSystem/StudentRecordManagementSystem/Controllers/StudentController.cs
@@ -5,9 +5,21 @@ namespace StudentRecordManagementSystem.Controllers
 {
     public class StudentController : Controller
     {
+        StudentDataAccessLayer studentDataAccessLayer = null;
+        public StudentController()
+        {
+            studentDataAccessLayer = new StudentDataAccessLayer();
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Student> students= studentDataAccessLayer.GetAllStudent();
+            return View(students);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Student student = studentDataAccessLayer.GetStudentData(id);
+            return View(student);
         }
 
         // GET: Student/Create
@@ -25,8 +37,7 @@ namespace StudentRecordManagementSystem.Controllers
         {
             try
             {
-                StudentDataAccessLayer studentdal = new StudentDataAccessLayer();
-                studentdal.AddStudent(student);
+                studentDataAccessLayer.AddStudent(student);
 
                 //Once  Student Record Created it will redirect to Index Page
                 return RedirectToAction(nameof(Index));
@@ -36,6 +47,52 @@ namespace StudentRecordManagementSystem.Controllers
                 return View();
             }
 
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Student student = studentDataAccessLayer.GetStudentData(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            try
+            {
+                // TODO: Add update logic here  
+                studentDataAccessLayer.UpdateStudent(student);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Student student = studentDataAccessLayer.GetStudentData(id);
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Student student)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                studentDataAccessLayer.DeleteStudent(student.Id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
